b0e18f30a88ba4b57d6583b93e6729f5012f6a47
Josiah King <josiahaccounts@gmail.com>
2025-08-13 20:48:32 +0100

    Added static and dynamic content handling, connected to remote serverless db

--- app/dashboard/page.tsx
+++
@@ -1,4 +0,0 @@
-export default function Page() {
-
-    return <h2>Dashboard page</h2>;
-}
--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,8 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import clsx from 'clsx';
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -14,18 +16,18 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  const { numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices } = await fetchCardData();
+
   return (
     <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
@@ -34,15 +36,26 @@ export function Card({
   title,
   value,
   type,
+  bgClass,
 }: {
   title: string;
   value: number | string;
   type: 'invoices' | 'customers' | 'pending' | 'collected';
+  bgClass?: string
 }) {
   const Icon = iconMap[type];
 
   return (
-    <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
+    <div className={clsx(
+      "rounded-xl bg-gray-50 p-2 shadow-sm",
+      {
+        'bg-blue-50': type === 'collected',
+        'bg-yellow-50': type === 'pending',
+        'bg-green-50': type === 'customers',
+        'bg-gray-100': type === 'invoices',
+        bgClass,
+      }
+    )}>
       <div className="flex p-4">
         {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
