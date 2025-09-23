5034bebd9768c79afe90dc4c817117f2a3d989c6
Josiah King <josiahaccounts@gmail.com>
2025-09-23 21:01:52 +0100

    added validation and accessibility to create-invoice form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,21 +6,43 @@ import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: "Please select a customer."
+    }),
+    amount: z.coerce
+    .number()
+    .gt(0, { message: "Please enter an amount greater than $0."}),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: "Please select an invoice status."
+    }),
     date: z.string(),
 });
 const CreateInvoice = FormSchema.omit({id: true, date: true});
 const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
 
-export async function createInvoice(formdata: FormData) {
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string|null;
+}
+
+export async function createInvoice(prevState: StaticRange, formdata: FormData) {
     const rawFormData = {
         customerId: formdata.get('customerId'),
         amount: formdata.get('amount'),
         status: formdata.get('status'),
     };
-    const {customerId, amount, status} = CreateInvoice.parse(rawFormData);
+    const validatedFields = CreateInvoice.safeParse(rawFormData);
+    if(!validatedFields.success){
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: "Missing fields: Failed to create invoice"
+        };
+    }
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
     try{
